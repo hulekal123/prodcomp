311f0f8fd858f90b45e347afb11fe84a8ec95619
Preeti Hulekal <hulekal.preeti@gmail.com>
2020-10-10 12:32:45 +0200

    init

---
+++ src/main/java/com/relayr/productcomparision/ProductcomparisionApplication.java
@@ -0,0 +1,13 @@
+package com.relayr.productcomparision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcomparisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductcomparisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relayr/productcomparision/ProductcomparisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relayr.productcomparision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcomparisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
